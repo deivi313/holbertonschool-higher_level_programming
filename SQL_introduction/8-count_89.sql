ad94eea2546c7720323b41a7967fd2b686618d18
deivi313 <deiviboja@gmail.com>
2026-09-10 18:09:58 +0200

    task 9

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,3 +1,2 @@
 --Display number of records
-INSERT INTO first_table VALUES  (89, "Best School");
 SELECT COUNT(*) FROM first_table WHERE id = 89;
---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+--Full table creation
+CREATE TABLE IF NOT EXISTS second_table (
+  id INT,
+ name VARCHAR(256),
+ score INT
+);
+INSERT INTO second_table (name ,score)
+VALUES 
+("John", 10),
+("Alex",3),
+("Bob", 14),
+("George", 8);
